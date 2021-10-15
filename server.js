35a56004adb7bd5f88df1cef1ffa6433ae0b5624
Alice403 <alejandra403.arciniegas@hotmail.es>
2021-10-15 17:32:12 -0500

    autenticacion backend

--- server.js
+++ server.js
@@ -5,12 +5,29 @@ import dotenv from "dotenv";
 import rutasUsuarios from './views/usuarios/rutas.js';
 import rutasVentas from './views/ventas/rutas.js';
 import rutasProductos from './views/productos/rutas.js'
+import jwt from 'express-jwt';
+import jwks from 'jwks-rsa';
 
 dotenv.config({path: "./.env"});
 
 const app = Express();
 app.use(Express.json());
 app.use(Cors());
+
+var jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://go-code.us.auth0.com/.well-known/jwks.json'
+  }),
+  audience: 'api-autenticacion-cuadernia',
+  issuer: 'https://go-code.us.auth0.com/',
+  algorithms: ['RS256']
+});
+
+app.use(jwtCheck);
+
 app.use(rutasUsuarios);
 app.use(rutasVentas);
 app.use(rutasProductos);
